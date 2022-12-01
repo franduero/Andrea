a27eae92fbd6788c6fa9f0530b95dca7cac92ce9
Fran Ramos <frankduero@gmail.com>
2022-12-01 22:30:19 +0100

    committed

---
+++ assets/js/index.js
@@ -0,0 +1,36 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const openMenu = document.getElementById("open");
+  const closeMenu = document.getElementById("close");
+  const mobileMenu = document.getElementById("mobile-menu")
+
+  openMenu.addEventListener("click", () => {
+    console.log("ABIERTO")
+    mobileMenu.style.height="auto"
+    openMenu.style.display="none"
+    closeMenu.style.display="flex"
+  });
+
+  closeMenu.addEventListener("click", () => {
+    console.log("ABIERTO")
+    mobileMenu.style.height="0"
+    openMenu.style.display="flex"
+    closeMenu.style.display="none"
+  });
+
+  window.addEventListener("resize", () => {
+    if(window.innerWidth >= 980){
+        openMenu.style.display="none"
+        closeMenu.style.display="none"
+        mobileMenu.style.height="0"
+    } else {
+        if(openMenu.style.height == "auto"){
+            closeMenu.style.display="flex"
+            openMenu.style.display="none"
+        } else{
+            mobileMenu.style.height="0"
+            closeMenu.style.display="none"
+            openMenu.style.display="flex"
+        }
+    }
+  })
+});
