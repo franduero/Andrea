6a876d155215744a87edd25138ad24751f164045
FranRamos <frankduero@gmail.com>
2022-12-06 17:20:30 +0100

    committed

--- assets/js/header.js
+++ assets/js/header.js
@@ -5,12 +5,16 @@ document.addEventListener("DOMContentLoaded", () => {
 
   openMenu.addEventListener("click", () => {
     mobileMenu.style.height="180px"
+    mobileMenu.style.borderTop="1px solid black"
+    mobileMenu.style.borderBottom="1px solid black"
     openMenu.style.display="none"
     closeMenu.style.display="flex"
   });
 
   closeMenu.addEventListener("click", () => {
     mobileMenu.style.height="0"
+    mobileMenu.style.borderTop="1px solid transparent"
+    mobileMenu.style.borderBottom="1px solid transparent"
     openMenu.style.display="flex"
     closeMenu.style.display="none"
   });
