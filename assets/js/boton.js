82773bd36b8353553b2f1721bde48bc29e0dd63f
FranRamos <frankduero@gmail.com>
2022-12-08 19:47:11 +0100

    committed

---
+++ assets/js/boton.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+
+  var button = document.getElementById("move");
+  var bottones = document.getElementById('botones')
+  var quieto = document.getElementById('noMove');
+  var screenWidth = window.innerWidth;
+  var screenHeight = window.innerHeight;
+
+
+  var quitar = document.getElementById('header')
+  var quitar1 = document.getElementById('app')
+  var quitar2 = document.getElementById('footer')
+
+
+
+  button.addEventListener("click", function () {
+    var randomX = Math.random() * (screenWidth - 100);
+    var randomY = Math.random() * (screenHeight - 90);
+
+    button.style.left = randomX + "px";
+    button.style.top = randomY + "px";
+    button.className = 'moveer';
+  });
+
+  quieto.addEventListener('click', function () {
+    console.log('dasd')
+    bottones.style.display='none'
+    limpiar();
+
+  });
+  function limpiar() {
+    quitar.classList.remove('fuera');
+    quitar1.classList.remove('fuera');
+    quitar2.classList.remove('fuera');
+  }
+
+
+})
