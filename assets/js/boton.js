747e585c4dabcb7707703f3026be4cfff0b053fc
FranRamos <frankduero+github@gmail.com>
2022-12-21 20:26:13 +0100

    commit

--- assets/js/boton.js
+++ assets/js/boton.js
@@ -18,8 +18,8 @@ document.addEventListener("DOMContentLoaded", () => {
   quitar2.style.display = "none"
  
   fondoo.style.display= 'none'
-  /* bottones.style.display="none" */
-  /*
+ bottones.style.display="none" */
+  
   var countDownDate = new Date("Dec 31, 2022 22:00:00").getTime();
 // Update the count down every 1 second
 var x = setInterval(function() {
@@ -48,7 +48,7 @@ var x = setInterval(function() {
     document.getElementById("demo").style.display='none'
   }
 }, 1000);
- */
+
   button.addEventListener("click", function () {
     var randomX = Math.random() * (screenWidth - 100);
     var randomY = Math.random() * (screenHeight - 90);
